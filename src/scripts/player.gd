8298622d12dfd653456bc095c1cdf61a7c37840d
VernalUmbrella <vernalumbrella@gmail.com>
2025-03-05 22:57:25 -0500

    Clean up Player script
    snake_case is the convention for variables in GDScript.
    We preload the scene once and cache it into a constant.
    Input actions are also snake_case by convention.

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,31 +1,35 @@
+class_name Player
 extends Node2D
 
 const BASE_ROTATE_SPEED = 60
 const BOOST_MULTIPLIER = 3
+const ProjectileScene = preload("res://scenes/player_projectile.tscn")
 
 @onready var muzzle = $Muzzle
-@onready var gameNode = $".."
+@onready var game_node = $".."
 
-var rotateSpeed = BASE_ROTATE_SPEED
-var projectile = "res://scenes/player_projectile.tscn"
+var speed_multiplier: float = 1
 
 func _input(event: InputEvent) -> void:
-	var delta = get_process_delta_time()
-	if Input.is_action_pressed("Boost"): rotateSpeed = BASE_ROTATE_SPEED * BOOST_MULTIPLIER
-	else: rotateSpeed = BASE_ROTATE_SPEED
-	if Input.is_action_just_pressed("Shoot"):
-		_shoot()
+	if event.is_action_pressed("boost"):
+		speed_multiplier = BOOST_MULTIPLIER
+	if event.is_action_released("boost"):
+		speed_multiplier = 1
+	if event.is_action_pressed("shoot"):
+		shoot()
+
 
 func _process(delta: float) -> void:
-	rotate(deg_to_rad(rotateSpeed) * delta)
+	rotate(deg_to_rad(BASE_ROTATE_SPEED) * speed_multiplier * delta)
 
-func _shoot(): #create a projectile
-	var newProjectile = load(projectile).instantiate()
-	newProjectile.global_position = muzzle.global_position
-	newProjectile.rotation = rotation
-	gameNode.add_child(newProjectile)
+
+func shoot(): #create a projectile
+	var new_projectile = ProjectileScene.instantiate()
+	new_projectile.global_position = muzzle.global_position
+	new_projectile.rotation = rotation
+	game_node.add_child(new_projectile)
 	
 
 
-func _on_hitbox_area_entered(area: Area2D) -> void: #hitbox
-	pass # Replace with function body.
+func _on_hitbox_area_entered(_area: Area2D) -> void: #hitbox
+	print("ACK I'VE BEEN HIT")
