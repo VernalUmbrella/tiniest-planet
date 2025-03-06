992467feaeb00e947f8d0f51a17f48a328c43667
VernalUmbrella <vernalumbrella@gmail.com>
2025-03-06 00:29:57 -0500

    Implement enemy motion
    Enemies are technically positioned at the planet, just like the Player,
    and have an offset object called the Orbit. To revolve the Orbit, rotate
    the Enemy. To pull the Orbit in, increase the Orbit's y-position.

--- src/scripts/enemy.gd
+++ src/scripts/enemy.gd
@@ -1,6 +1,16 @@
 class_name Enemy
-extends Area2D
+extends Node2D
 
+@export var fall_speed: float = 8.0
+@export_range(0, 360, 0.1, "radians_as_degrees") var angular_speed: float = 0.1
+
+@onready var orbit: Area2D = $Orbit
+
+
+func _process(delta: float) -> void:
+	orbit.position.y += fall_speed * delta
+	rotation += angular_speed * delta
+	
 
 func die() -> void:
 	# TODO: sound effect
@@ -8,8 +18,7 @@ func die() -> void:
 	queue_free()
 
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_orbit_area_entered(area:Area2D) -> void:
 	# TODO: differentiate between crashing into the planet and getting hit by a bullet
 	die()
-
 
