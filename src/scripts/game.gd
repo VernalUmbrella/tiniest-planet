001146c6ce61800f99a3e30b88b66af9958f9b6a
VernalUmbrella <vernalumbrella@gmail.com>
2025-03-06 00:05:01 -0500

    Free bullets outside game window and make Game play music

--- src/scripts/game.gd
+++ src/scripts/game.gd
@@ -5,4 +5,16 @@ const WINDOW_WIDTH := 256
 const WINDOW_HEIGHT := 256
 const SCREEN_CENTER := Vector2(WINDOW_WIDTH, WINDOW_HEIGHT) / 2
 
-# TODO: Delete bullets that aren't in the playable area
+@onready var music_player: AudioStreamPlayer = $MusicPlayer
+
+func _ready() -> void:
+	start_round()
+
+
+func start_round() -> void:
+	music_player.play()
+
+
+func _on_playable_area_area_exited(area: Area2D) -> void:
+	print("DELETING:" + str(area))
+	area.queue_free()
